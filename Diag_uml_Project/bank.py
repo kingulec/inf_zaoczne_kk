ef82d9f213b511794664259aef7e5595d4190c08
kingulec <kinga_kociszewska@interia.pl>
2023-02-10 15:51:33 +0100

    bank app: add Klient and Bank classes

---
+++ Diag_uml_Project/bank.py
@@ -0,0 +1,30 @@
+import  random
+
+class Klient:
+    def __init__(self):
+        self.imie = imie
+        self.nazwisko = nazwisko
+        self.pesel = pesel
+        self.konto = Konto
+
+    def display_data():
+        print(f"Imie= {self.imie}\n"
+              f"Nazwisko= {self.nazwisko}\n"
+              f"Pesel= {self.pesel}")
+
+class Bank:
+    def __init__(self):
+        self.nazwa_banku = nazwa
+        self.adres_głowny = adres
+
+    @staticmethod
+    def Autoryzacja(numer_transakcji, status=True)
+        if status == True:
+            print("Autoryzacja transakcji " + numer_transakcji)
+            return True
+        print("Transakcja się nie powiodła")
+        return False
+
+# class Transakcja:
+#     def __init__(type):
+#         self.numer_transakcji = type + random.
